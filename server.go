a7a45716269c904bff64923a0094eac081e9486c
Jon Newton <newt@hey.com>
2024-12-30 00:22:02 -0500

    Change to nginx and certbot.  Simplify config.

--- server.go
+++ server.go
@@ -4,11 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"net"
 	"net/http"
 	"os"
 	"path/filepath"
-	"strconv"
+	"strings"
 )
 
 func checkDir(serverDir string, dirName string) (fullDir string, err error) {
@@ -26,10 +25,7 @@ func checkDir(serverDir string, dirName string) (fullDir string, err error) {
 type ServerConfig struct {
 	ServerDir        string `json:"serverDir"`
 	ServerListenAddr string `json:"serverListenAddr"`
-	ServerListenPort int    `json:"serverListenPort"`
 	DbFile           string `json:"dbFile"`
-	CertFile         string `json:"certFile"`
-	KeyFile          string `json:"keyFile"`
 	productionMode   bool
 }
 
@@ -52,17 +48,14 @@ func readConfig(jsonFilePath string) (ServerConfig, error) {
 	return config, nil
 }
 
-func RunDevServer(serverDir string, serverPort int) (rc int, err error) {
-	log.Println("enter RunDevServer", serverDir, serverPort)
+func RunDevServer(serverDir string, listenAddr string) (rc int, err error) {
+	log.Println("enter RunDevServer", serverDir, listenAddr)
 	defer log.Println("exit RunDevServer")
 
 	config := ServerConfig{
 		ServerDir:        serverDir,
-		ServerListenAddr: "",
-		ServerListenPort: serverPort,
+		ServerListenAddr: listenAddr,
 		DbFile:           filepath.Join(serverDir, "tapedeck.db"),
-		CertFile:         "",
-		KeyFile:          "",
 		productionMode:   false,
 	}
 
@@ -95,33 +88,15 @@ func start(config ServerConfig) (rc int, err error) {
 		return 148, dirErr
 	}
 
+	log.Println("validate server listen address")
+	if strings.Count(config.ServerListenAddr, ":") != 1 {
+		return 92, fmt.Errorf("invalid server listen address: %v", config.ServerListenAddr)
+	}
+
 	log.Println("validate sqlite db file path")
 	_, dbErr := os.Stat(config.DbFile)
 	if dbErr != nil {
 		return 155, dbErr
-	}
-
-	if config.CertFile != "" {
-		log.Println("validate certificate file path")
-		_, certErr := os.Stat(config.CertFile)
-		if certErr != nil {
-			return 163, certErr
-		}
-	}
-
-	if config.KeyFile != "" {
-		// TODO: check permissions on this file and panic if not strict enough.
-		log.Println("validate key file path")
-		_, keyErr := os.Stat(config.KeyFile)
-		if keyErr != nil {
-			return 176, keyErr
-		}
-	}
-
-	if config.productionMode {
-		if config.CertFile == "" || config.KeyFile == "" {
-			return 213, fmt.Errorf("production server requires TLS but CertFile or KeyFile empty")
-		}
 	}
 
 	log.Println("using sqlite file", config.DbFile)
@@ -152,38 +127,17 @@ func start(config ServerConfig) (rc int, err error) {
 
 	log.Println("server verification complete")
 
-	//TODO: implement ACME support to allow for certbot autorenew.
-	//Always run a server on HTTP, port 80 and serve ./.well-known/acme-challenge/
-	//https://eff-certbot.readthedocs.io/en/stable/using.html#webroot
-
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(staticDir))))
 	http.HandleFunc("/", MakeRootHandler(tmplEngine))
 	http.HandleFunc("/list", MakeListHandler(db, tmplEngine))
 	http.HandleFunc("/playback", MakePlaybackHandler(db, tmplEngine))
 	http.HandleFunc("/record", MakeRecordHandler(db, tmplEngine))
 
-	listenAddress := config.ServerListenAddr + ":" + strconv.Itoa(config.ServerListenPort)
-
-	if config.productionMode {
-		log.Println("production server starting in HTTPS mode. listen address is", listenAddress)
-		listener, listenErr := net.Listen("tcp4", listenAddress)
-		if listenErr != nil {
-			return 292, listenErr
-		}
-
-		err = http.ServeTLS(listener, nil, config.CertFile, config.KeyFile)
-		if err != nil {
-			return 297, err
-		} else {
-			return 0, nil
-		}
+	log.Println("server starting on", config.ServerListenAddr)
+	err = http.ListenAndServe(config.ServerListenAddr, nil)
+	if err != nil {
+		return 286, err
 	} else {
-		log.Println("dev server starting in HTTP mode. listen address is", listenAddress)
-		err = http.ListenAndServe(listenAddress, nil)
-		if err != nil {
-			return 286, err
-		} else {
-			return 0, nil
-		}
+		return 0, nil
 	}
 }
